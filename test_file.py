781989ca1442e6c1a99206d5fad8282de9000b1b
Zakhar <Zakhar.Zverev2101@yandex.ru>
2024-02-13 14:35:24 +0300

    commit

--- test_file.py
+++ test_file.py
@@ -1,1 +1,12 @@
+class User:
+    def __init__(self, name, surname, ID, languages, sex, age):
+        self.name=name
+        self.surname=surname
+        self.ID=ID
+        self.languages=languages
+        self.sex=sex
+        self.age=age
+
+User1=['Nika', 'Kuznetsova', 1, ["english",'french'], 'Female', 31]
+User2=['Kolya', 'Veselov', 2, ['german'], 'Male', 22]
 
