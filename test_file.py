0d44d0922bdc33c7e568acb80ebda192c55ea9ad
Zakhar <Zakhar.Zverev2101@yandex.ru>
2024-02-13 15:28:54 +0300

    commit

--- test_file.py
+++ test_file.py
@@ -9,6 +9,16 @@ class User:
     def description(self):
         print(f'{self.name, self.surname} is our student!')
 
+class Teacher:
+    def __init__(self, name, surname, T_ID, languages, sex, age):
+        self.name=name
+        self.surname=surname
+        self.T_ID=T_ID
+        self.languages=languages
+        self.sex=sex
+        self.age=age
+
 User1=['Nika', 'Kuznetsova', 1, ["english",'french'], 'Female', 31]
 User2=['Kolya', 'Veselov', 2, ['german'], 'Male', 22]
 
+Teacher1=['Alla', 'Makarovna', 1, ["russian", 'english'], 'Female', 51]
